22d8c659d91077c341b1f7e0bbd88a313e696ea4
Mohamed Gouda <goda61@gmail.com>
2024-04-29 23:50:38 +0300

    Add functions-- training

--- 01 Python & sql/05-Functions.py
+++ 01 Python & sql/05-Functions.py
@@ -8,3 +8,7 @@ name = 'Mohamed gouda'
 print(name.upper())
 print(name.lower())
 print(name.title())
+print(name.split(' '))
+print(name.replace('gouda','Atalla'))
+print(len(name))
+print(len(name.replace(' ' ,'')))
